948a9f7f9040749f9d5ada9c756dbabd85156079
RaZzV2 <razzrozo10@gmail.com>
2022-06-23 16:44:11 +0300

    login screen main screen and added function for 'register button'

--- app/src/main/java/com/example/badhabits/LoginActivity.java
+++ app/src/main/java/com/example/badhabits/LoginActivity.java
@@ -2,13 +2,29 @@ package com.example.badhabits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        textView = (TextView)findViewById(R.id.textView2);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 }
--- app/src/main/java/com/example/badhabits/MainActivity.java
+++ app/src/main/java/com/example/badhabits/MainActivity.java
@@ -3,14 +3,14 @@ package com.example.badhabits;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
-    
 }
