cccfe374b19966ba79838097ebdbdfe22bb479d9
RaZzV2 <razzrozo10@gmail.com>
2022-06-23 18:35:05 +0300

    added login functionality

--- app/src/main/java/com/example/badhabits/LoginActivity.java
+++ app/src/main/java/com/example/badhabits/LoginActivity.java
@@ -5,15 +5,41 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     TextView textView;
+    EditText emailView;
+    EditText passwordView;
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        emailView = (EditText)findViewById(R.id.edtEmail);
+        passwordView = (EditText)findViewById(R.id.edtPass);
+
+        button = (Button)findViewById(R.id.Login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String pass = passwordView.getText().toString();
+                String email = emailView.getText().toString();
+                if(pass.equals("razz") && email.equals("razz")){
+                    Intent intent = new Intent(LoginActivity.this,HomeActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this, "Username or password wrong!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         textView = (TextView)findViewById(R.id.textView2);
         textView.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/badhabits/RegisterActivity.java
+++ app/src/main/java/com/example/badhabits/RegisterActivity.java
@@ -2,13 +2,26 @@ package com.example.badhabits;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        textView = (TextView)findViewById(R.id.textView2);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =  new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
