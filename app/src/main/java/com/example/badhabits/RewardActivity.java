37988a66bebb0cad5f2556d12194a4d7caa82cd4
RaZzV2 <razzrozo10@gmail.com>
2022-06-29 16:11:34 +0300

    ranking system added

--- app/src/main/java/com/example/badhabits/RewardActivity.java
+++ app/src/main/java/com/example/badhabits/RewardActivity.java
@@ -22,17 +22,7 @@ public class RewardActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     RecyclerView.Adapter programAdapter;
     RecyclerView.LayoutManager layoutManager;
-    /*String[] programNameList = {"C", "C++", "Java", "Android", "HTML5", "CSS3", "JavaScript", "jQuery", "Bootstrap", "PHP",
-            "MySQL", "CodeIgniter", "React", "NodeJS", "AngularJS", "PostgreSQL", "Python", "C#", "Wordpress", "GitHub"};*/
-    String[] programDescriptionList = {"C Description", "C++ Description", "Java Description",
-            "Android Description", "HTML5 Description",
-            "CSS3 Description", "JavaScript Description", "jQuery Description",
-            "Bootstrap Description", "PHP Description", "MySQL Description",
-            "CodeIgniter Description", "React Description", "NodeJS Description",
-            "AngularJS Description", "PostgreSQL Description", "Python Description",
-            "C# Description", "Wordpress Description", "GitHub Description"};
-    // Define an integer array to hold the image recourse ids
-    int[] programImages = {R.drawable.gold_medal, R.drawable.cplusplus,
+    int[] programImages = {R.drawable.c, R.drawable.cplusplus,
             R.drawable.java, R.drawable.android, R.drawable.html5,
             R.drawable.css3, R.drawable.javascript, R.drawable.jquery,
             R.drawable.bootstrap, R.drawable.php, R.drawable.mysql,
@@ -54,9 +44,18 @@ public class RewardActivity extends AppCompatActivity {
         toolbar.setBackgroundColor(SelectColorActivity.getColor(this));
         getWindow().setStatusBarColor(SelectColorActivity.getColor(this));
         String[] programNameList = new String[habitModels.size()];
+        String[] programDescriptionList = new String[habitModels.size()];
         for(int index=0;index<programNameList.length;++index)
         {
             programNameList[index] = (users.get(habitModels.get(index).getUserId()).getUsername());
+        }
+        LocalDate localDate = LocalDate.now();
+        for(int index=0;index<programDescriptionList.length;++index)
+        {
+            programDescriptionList[index] = habitModels.get(index).getHabit();
+            programDescriptionList[index] += "\n";
+            programDescriptionList[index] += String.valueOf(DAYS.between(habitModels.get(index).getDate(),localDate));
+            programDescriptionList[index] += " days";
         }
         recyclerView = findViewById(R.id.rvProgram);
         recyclerView.setHasFixedSize(true);
