4269852efbda9ce23f5e58911ce579b938c1248e
Cristian George <cfieraru29@yahoo.com>
2022-06-29 22:32:22 +0300

    added rewards for users that got rid of a bad habit a long time ago

--- app/src/main/java/com/example/badhabits/RewardActivity.java
+++ app/src/main/java/com/example/badhabits/RewardActivity.java
@@ -18,43 +18,54 @@ import java.util.Comparator;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class RewardActivity extends AppCompatActivity {
-    DBHelper myDB = new DBHelper(RewardActivity.this);
+    DBHelper dbHelper = new DBHelper(RewardActivity.this);
     RecyclerView recyclerView;
     RecyclerView.Adapter programAdapter;
     RecyclerView.LayoutManager layoutManager;
-    int[] programImages = {R.drawable.c, R.drawable.cplusplus,
-            R.drawable.java, R.drawable.android, R.drawable.html5,
-            R.drawable.css3, R.drawable.javascript, R.drawable.jquery,
-            R.drawable.bootstrap, R.drawable.php, R.drawable.mysql,
-            R.drawable.codeigniter, R.drawable.react, R.drawable.nodejs,
-            R.drawable.angularjs, R.drawable.postgresql, R.drawable.python,
-            R.drawable.csharp, R.drawable.wordpress, R.drawable.github};
 
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayList<UserModel> users = myDB.getAllUsers();
-        ArrayList<BadHabitModel> habitModels = myDB.getAllHabits();
+        ArrayList<UserModel> users = dbHelper.getAllUsers();
+        ArrayList<BadHabitModel> habitModels = dbHelper.getAllHabits();
         Collections.sort(habitModels, new Sorting());
         setContentView(R.layout.activity_reward);
 
         toolbar = findViewById(androidx.appcompat.R.id.action_bar);
         toolbar.setBackgroundColor(SelectColorActivity.getColor(this));
         getWindow().setStatusBarColor(SelectColorActivity.getColor(this));
+
         String[] programNameList = new String[habitModels.size()];
         String[] programDescriptionList = new String[habitModels.size()];
-        for (int index = 0; index < programNameList.length; ++index) {
-            programNameList[index] = (users.get(habitModels.get(index).getUserId()).getUsername());
-        }
+        int[] programImages = new int[habitModels.size()];
+
         LocalDate localDate = LocalDate.now();
         for (int index = 0; index < programDescriptionList.length; ++index) {
-            programDescriptionList[index] = habitModels.get(index).getHabit();
+
+            BadHabitModel habitModel = habitModels.get(index);
+            long days = DAYS.between(habitModel.getDate(), localDate);
+
+            programNameList[index] = "\n";
+            programNameList[index] += (users.get(habitModel.getUserId()).getUsername());
+
+            programDescriptionList[index] = habitModel.getHabit();
             programDescriptionList[index] += "\n";
-            programDescriptionList[index] += String.valueOf(DAYS.between(habitModels.get(index).getDate(), localDate));
-            programDescriptionList[index] += " days";
+            programDescriptionList[index] += String.valueOf(days);
+            programDescriptionList[index] += " days ago";
+
+            if (days >= 30 && days < 60) {
+                programImages[index] = R.drawable.bronze_medal;
+            } else if (days >= 60 && days < 90) {
+                programImages[index] = R.drawable.silver_medal;
+            } else if (days >= 90) {
+                programImages[index] = R.drawable.gold_medal;
+            } else {
+                programImages[index] = R.drawable.empty_medal2;
+            }
         }
+
         recyclerView = findViewById(R.id.rvProgram);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(this);
